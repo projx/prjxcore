d52e9e9b4e643cec1d83b97e658f3c936071001f
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-10-25 00:17:35 +0100

    updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='prjxcore',
-     version='0.34' ,
+     version='0.35' ,
      author="prjx",
      author_email="kelvin@prjx.uk",
      description="A collection of base-classes used for common tasks, such as Config loading, logging etc",
